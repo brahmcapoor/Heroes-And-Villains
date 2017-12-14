03bf8e9cd588cb31491865a2eecac4e3778d1a50
Michael Andreas Troute <mtroute@stanford.edu>
2017-12-13 22:21:25 -0800

    My hands are typing words

--- src/constants.py
+++ src/constants.py
@@ -10,3 +10,7 @@ MOVIE_CONVERSATIONS_FILENAME = "{}/movie_conversations.txt".format(DATASET_PATH)
 LABELLED_DATA_FILENAME = "{}/movie_pa_labels.txt".format(DATASET_PATH)
 SCRIPT_URL_FILENAME = "{}/raw_script_urls.txt".format(DATASET_PATH)
 LINES_FILENAME = "{}/movie_lines.txt".format(DATASET_PATH)
+
+SENTIMENT_DIR_PATH = '../sentiments/{}_segments'
+SENTIMENT_OUTPUT_PATH = '../sentiments/{}_segments/{}_sentiments.txt'
+PLOT_DIR_PATH = '../plots/{}_segments/k_{}/filter_{}/'
